c35d7f6eccd4d7f4ba830fcc6613efa97fa69a49
rajitbanerjee <rajit.banerjee@ucdconnect.ie>
2020-02-10 19:29:52 +0000

    change naming order to match given algorithm

--- p3/Hanoi.java
+++ p3/Hanoi.java
@@ -9,18 +9,19 @@ public class Hanoi {
      * @param dest the destination tower
      * @param aux the auxiliary tower to help move the disk
      */
-    public static void towersOfHanoi(int n, String src, String aux, String dest) {
+    public static void towersOfHanoi(int n, String src, String dest, String aux) {
         if (n == 1) {
             System.out.println("Move disk from " + src + " to " + dest);
         } else {
-            towersOfHanoi(n - 1, src, dest, aux);
+            towersOfHanoi(n - 1, src, aux, dest);
             System.out.println("Move disk from " + src + " to " + dest);
-            towersOfHanoi(n - 1, aux, src, dest);
+            towersOfHanoi(n - 1, aux, dest, src);
         }
     }
+    
     public static void main(String[] args) {
         int n = 3;
-        towersOfHanoi(n, "SOURCE","AUXILIARY", "DESTINATION");
+        towersOfHanoi(n, "SOURCE", "DESTINATION", "AUXILIARY");
         System.out.println("Number of moves: " + (int)(Math.pow(2, n) - 1));
     }
 }

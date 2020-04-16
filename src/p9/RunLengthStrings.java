bb30a20a31a911e509f24b04588398d22290ab60
rajitbanerjee <rajit.banerjee@ucdconnect.ie>
2020-04-17 00:59:36 +0100

    :sparkles: add compiled classes for java programs used in the command line

---
+++ src/p9/RunLengthStrings.java
@@ -0,0 +1,59 @@
+package p9;
+
+/*
+ *  Compilation:  javac p9/RunLengthStrings.java
+ *  Execution:    java p9/RunLengthStrings someString
+ *  Dependencies: p9/StdOut.java
+ */
+
+import util.StdOut;
+
+/**
+ * The {@code RunLengthStrings} class provides static methods for compressing
+ * and expanding String input.
+ * E.g. aaaabbbbbcdefff is compressed to a4b5cdef3
+ *
+ * @author Rajit Banerjee
+ */
+public class RunLengthStrings {
+    /**
+     * Compresses the input String and writes the
+     * results to standard output.
+     *
+     * @param input the String to compress
+     */
+    public static void compress(String input) {
+        int counter = 1;
+        input += " ";
+        if (input.length() > 1) {
+            for (int i = 1; i < input.length(); i++) {
+                if (input.charAt(i) == input.charAt(i - 1)) {
+                    counter++;
+                } else {
+                    StdOut.print(input.charAt(i - 1));
+                    if (counter > 1) {
+                        StdOut.print(counter);
+                    }
+                    counter = 1;
+                }
+            }
+        } else {
+            StdOut.print(input);
+        }
+    }
+
+    /**
+     * Sample client that calls {@code compress()} on the command line
+     * argument String input.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 0) {
+            System.out.println("Original string:\n" + args[0]);
+            System.out.println("Compressed string:");
+            compress(args[0]);
+        }
+    }
+
+}
